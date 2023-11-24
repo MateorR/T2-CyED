b0e1b0e6b51d44f46dc759a2436a0e259c963010
Alejandro Quiñones Caicedo <50637445+Legnatbird@users.noreply.github.com>
2023-11-24 03:23:27 -0500

    Start to implement the new ui

---
+++ src/main/java/com/t2/cyed/Actions.java
@@ -0,0 +1,4 @@
+package com.t2.cyed;
+
+public class Actions {
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,8 @@
 module ui {
   requires javafx.controls;
   requires javafx.fxml;
-  requires javafx.media;
   requires java.desktop;
-  opens ui to javafx.fxml;
-  exports ui;
-  exports ui.Sprites;
-  opens ui.Sprites to javafx.fxml;
+
+  opens com.t2.cyed to javafx.fxml;
+  exports com.t2.cyed;
 }
